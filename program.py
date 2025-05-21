958d08c4814ea8c6a89a1588c1bda10ef20b7e5b
haileyhendrickson <hhendrickson@energystrat.com>
2025-05-21 13:58:29 -0600

    updating API now that I can interact with it more

--- program.py
+++
@@ -1,7 +0,0 @@
-# ask user for what report type they are looking for (XML Name. EX: PRC_LMP)
-    # use an if statement to filter to the correct URL format
-report_type = 'PRC_LMP' # ('What report are you looking for? Put in the code: ')
-
-if report_type == 'PRC_LMP':
-    # can group to use all nodes, or can specify one or multiple nodes
-    print()
--- query.py
+++ query.py
@@ -3,19 +3,65 @@ import requests
 from zipfile import ZipFile, BadZipFile
 from io import BytesIO
 
+# User inputs/ setting parameters
+queryname = input('Select Report Type: ') # how to make a dropdown later?
+if queryname == 'PRC_LMP':
+    market_run_id = 'DAM'
+if queryname == 'PRC_SPTIE_LMP':
+    market_run_id = 'DAM'
+if queryname == 'PRC_INTVL_LMP':
+    market_run_id = 'RTM'
+if queryname == 'PRC_HASP_LMP':
+    market_run_id = 'HASP'
+if queryname == 'PRC_RTPD_LMP':
+    market_run_id = 'PRC_RTPD_LMP'
+if queryname == 'PRC_DS_REF':
+    market_run_id = '' # NO MARKET ID FOR THIS ONE   
+if queryname == 'PRC_CD_INTVL_LMP':
+    market_run_id = 'RTM'
+if queryname == 'PRC_RTM_LAP':
+    market_run_id = 'RTM'
+
+startdate = input('Enter a start date (YYYY/MM/DD): ').replace('/', '')
+enddate = input('Enter an end date (YYYY/MM/DD): ').replace('/', '')
+timeSelect = input('Do you want to start at a specific time? ') # add in time settings later
+if timeSelect == 'Y':
+    starttime = input('Enter Start time (hr:mn): ')
+    starttime = f'{starttime}-0000'
+    endtime = input('Enter End time (hr:mn): ')
+    endtime = f'{endtime}-0000'
+if timeSelect == 'N':
+    starttime = '00:00-0000'
+    endtime = '00:00-0000'
+startdatetime = startdate+'T'+starttime
+enddatetime = enddate+'T'+endtime
+
 base_url = "http://oasis.caiso.com/oasisapi/SingleZip"
 
-# Define your query parameters as a dictionary
-params = {
-    "resultformat": "6",
-    "queryname": "PRC_LMP",
-    "startdatetime": "20250101T07:00-0000",
-    "enddatetime": "20250101T08:00-0000",
-    "market_run_id": "DAM",
-    "version": "1",
-    "grp_type": "ALL_APNODES"  # Optional parameter (can vary by query)
+node = input('Specify a node. Separate multiple nodes with a comma. Leave blank for all nodes: ')
+if node == '': # means they want all nodes
+    params = {
+    "resultformat": 6, # should always be this- creates a CSV
+    "queryname": queryname, # determines what kind of data pull we want
+    "startdatetime": startdatetime,
+    "enddatetime": enddatetime,
+    "market_run_id": market_run_id,
+    "version": "1", # version 1 is the oldest and the safest I think
+    "grp_type": "ALL_APNODES"
+}
+if node != '':
+    node = node.replace(' ', '') # removing any spaces
+    params = {
+    "resultformat": 6, # should always be this- creates a CSV
+    "queryname": queryname, # determines what kind of data pull we want
+    "startdatetime": startdatetime,
+    "enddatetime": enddatetime,
+    "market_run_id": market_run_id,
+    "version": "1", # version 1 is the oldest and the safest I think
+    "node": node
 }
 
+# calling API
 try: # using this for now for error handling
     response = requests.get(base_url, params=params)
     with ZipFile(BytesIO(response.content)) as z:
@@ -24,9 +70,10 @@ try: # using this for now for error handling
                 df = pd.read_csv(f)
                 df.to_csv('test2.csv')
                 # print(df.info())
-                print(df.columns)
+                # print(df.columns)
                 # print(df['LMP_TYPE'].value_counts())
                 # print(df['XML_DATA_ITEM'].value_counts)
+                print("Done.")
 except BadZipFile:
     print('Response is not a ZIP')
     print(response.content.decode(errors='replace'))
