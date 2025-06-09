fb4311646e9fa18f6177805702c53ab391bd3325
haileyhendrickson <hhendrickson@energystrat.com>
2025-06-09 09:13:17 -0600

    cleaning all my files, renaming some, creating and updating documentation

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # CAISOAPI
-Repo for the CAISO API program for simplified and more efficient data pulling
+CAISO API program
+
+Internship project, creating a program to pull various price reports from the CAISO OASIS API (oasis.caiso.com). Purpose of the program is for simplified and more efficient data pulling for consultants and analysts at Energy Strategies.
+
+Installation
+(put instructions here)
+
+For GUI: use gui.py
+Backend code: backend.py
+Detailed documentation: documentation.txt (includes daily log and processes)
+Data dictionary: Notes about each report type under the 'prices' tab at the CASIO OASIS site. 
+All other files are part of the creation process, including learning and testing new python skills.
+
+Last updated: June 2025
 
 Workflow:
 WEEK: (to be completed by the end of that week)
-1- finish project concept 
-2- baseline CAISO API query in python and data dictionary
+1- energy market/company research and orientation
+2- finish project concept, baseline CAISO API query in python and data dictionary
 3- finish query bugs, build cleaning program, finish back-end
-4- push code to GUI of choice. finish “minimum viable product” (by lake powell week)
-5- add to product, get feedback from consultants on usability (learn about different analyses: basis and gen weighted)
-6- create supporting documentation and walk-through video, finish HMA report
+4- push code to GUI of choice. finish “minimum viable product”
+
+5- create supporting documentation, add to product, get feedback from consultants on usability 
+
+6- walk-through video, finish HMA report, (learn about different analyses: basis and gen weighted)
 7- 
 8- 
 9- 
--- gui.py
+++ gui.py
@@ -39,16 +39,13 @@ def backend(market_run_id, interval, startdate, enddate, sptie): # actually pull
                 }
 
         response = requests.get(url, params=params)
-        # print(response.url)
-
+        # print(response.url) # for testing purposes
         try:
             with ZipFile(BytesIO(response.content)) as z:
                 for filename in z.namelist():
                     with z.open(filename) as f:
                         df = pd.read_csv(f)
                         df.to_csv(f'pull#{counter}.csv')
-                        # if difference.days != 30:
-                        #     pass # adding this in so there aren't appending issues with small pulls
                         if '<?xml version="1.0" encoding="UTF-8"?>' in df.columns:
                             pass
                         else: # only appending files that pull- error handling for contingency dispatch
@@ -60,7 +57,7 @@ def backend(market_run_id, interval, startdate, enddate, sptie): # actually pull
     def cleanFile(filename):
         df = pd.read_csv(filename) # reading in file
 
-        if 'EFF_QTR_START_DT_GMT' in df.columns: # if the report type is reference prices
+        if 'EFF_QTR_START_DT_GMT' in df.columns: # if the report type is PRC_DS_REF (reference prices)
             df = df.sort_values(['EFF_QTR_START_DT_GMT']) # sort by EFF_QTR_START_DT_GMT
             df['EFF_QTR_START_DT_GMT'] = df['EFF_QTR_START_DT_GMT'].str.replace(':00-00:00', '').str.replace('T', ' ')
             df['EFF_QTR_END_DT_GMT'] = df['EFF_QTR_END_DT_GMT'].str.replace(':00-00:00','').str.replace('T',' ')
@@ -101,18 +98,17 @@ def backend(market_run_id, interval, startdate, enddate, sptie): # actually pull
         ('RTM', 60, 'N'): ('PRC_RTM_LAP', 6)
     }
     
-    queryname, version = map.get((market_run_id, interval, sptie), ('unknown', 'unknown')) # unknown is default val (means there is a error)
+    queryname, version = map.get((market_run_id, interval, sptie), ('unknown', 'unknown')) 
 
     startdate = datetime.strptime(startdate, '%m/%d/%y').date() # formats it to work with datetime package
     enddate = datetime.strptime(enddate, '%m/%d/%y').date()
     difference = enddate - startdate # counts days in between
     days = difference.days # making a counter for my loop bc .days is readonly
 
-
-
+    # logic that breaks the whole date range into chunks of 30 days to comply with the API
     files = [] # creating an empty list for my files
     df_list = []
-    counter = 0 # initializing, probably delete later
+    counter = 0 # initializing, used to label different files
     if difference.days > 30:
         while days > 0: # until we reach the end date
             if days < 30: # when we get below 30 days left
@@ -125,13 +121,12 @@ def backend(market_run_id, interval, startdate, enddate, sptie): # actually pull
             days -= 30 # updating this counter
             counter += 1 
             startdate = nextdate # update start date to be next date
-            # progressbar.step()
             time.sleep(10) # avoiding query limits
         for file in files: # if tabbed over, it cleans each pull before combining, but is bad at error handling
             cleanFile(file) 
             df_list.append(pd.read_csv(file))
 
-        df_combined = pd.concat(df_list, ignore_index=True)
+        df_combined = pd.concat(df_list, ignore_index=True) # combining 30 day chunks
         # mild cleaning for combined file
         cond_drop = ['Unnamed: 0.1', 'Unnamed: 0']
         conditional_drop = [col for col in cond_drop if col in df_combined.columns]
@@ -141,7 +136,6 @@ def backend(market_run_id, interval, startdate, enddate, sptie): # actually pull
         # root.after(3000, root.destroy) # quits 3 seconds after finishing
         status_lbl.configure(text='Finished!')
         root.update()
-
 
     if difference.days <= 30:
         print(f'Pulling data for {startdate} to {enddate}.')
@@ -154,31 +148,28 @@ def backend(market_run_id, interval, startdate, enddate, sptie): # actually pull
         root.update()
     
 # button functions
-def submit(): # handles user inputs and checks for SPTIE
+def submit(): # runs all of the backend code
     status_lbl.configure(text='Running...')
-    root.update()
+    root.update() # updating status label
     market_run_id=MRIDDropdown.get()
     interval=intvlDropdown.get()
         
-    market_run_id = market_run_id.upper()
-
     if interval != 'quarterly':
         interval = int(interval) # changing to an int if not quarterly
     
     backend(market_run_id, interval, startdate, enddate, sptie)
 
-
-def findStartDate(): # need to alter so I can choose start and end date with same calendar
+def findStartDate(): # for selecting the start date
     global startdate
     startdate = cal.get_date()
     startdate_label.configure(text=f'Start date: {startdate}')
 
-def findEndDate(): # need to alter so I can choose start and end date with same calendar
+def findEndDate(): # for selecting the end date
     global enddate
     enddate = cal.get_date()
     enddate_label.configure(text=f'End date: {enddate}')
 
-def select_output_file(): 
+def select_output_file(): # for selecting output file path
     global output_file_path
     directory = filedialog.askdirectory(title='Select output directory')
     if directory:
@@ -187,12 +178,10 @@ def select_output_file():
     else:
         output_file_label.configure(text='No directory selected yet')
 
-def sptie_toggle():
+def sptie_toggle(): # button to specify an SPTie report or not
     global sptie
     spite=spite.var.get()
 
-# def clicker(): # for progress bar
-#     progressbar.step()
 
 # tkinter program
 root = CTk() # initializing window
@@ -229,7 +218,7 @@ node_entry = CTkEntry(root, textvariable = node_var, font=('Arial',20), text_col
 
 sptiecheckbox = CTkCheckBox(root, text= 'SPTIE?', variable=sptie_var, offvalue='N', onvalue='Y', command=sptie_toggle)
 
-sub_btn=CTkButton(master=root,text = 'Submit', command = submit, corner_radius=32,fg_color='#162157', hover_color='#6D7DCF') # submit is calling 'submit' function (gets user inputs)
+sub_btn=CTkButton(master=root,text = 'Submit', command = submit, corner_radius=32,fg_color='#162157', hover_color='#6D7DCF') 
 
 output_file_button = CTkButton(root, text='Select Output File Path', command=select_output_file, corner_radius=32,fg_color='#162157', hover_color='#6D7DCF')
 output_file_label = CTkLabel(root, text='No file selected yet', font=('Arial',10), text_color='#04033A')
